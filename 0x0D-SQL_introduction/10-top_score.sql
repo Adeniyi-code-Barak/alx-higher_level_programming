c9008091c20507fa3a2cbd9dea43d26209770d1a
Adeniyi-code-Barak <adeniyiogundiran9@gmail.com>
2024-02-14 10:08:22 +0100

    SQL_INTRODUCTION

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table
+
+SELECT score, name
+	FROM second_table
+		ORDER BY score DESC, name;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state;
